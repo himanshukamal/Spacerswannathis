449a92bb52253f90a2650eff51649dc5aa4a8c23
Himanshu kamal <himanshukamal@192.168.1.7>
2022-07-13 01:21:45 +0530

    first commit

---
+++ components/effects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function effects() {
+  return (
+    <div className="cursor-not-allowed">
+      <div className="cursor rounded"></div>
+      <div className="cursor pointed"></div>
+    </div>
+  );
+}
